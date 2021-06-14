a1343c3b8026e814386ab28c3b88166846456f9b
Titan0932 <73685436+Titan0932@users.noreply.github.com>
2021-06-14 13:09:59 +0545

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,8 +4,11 @@ from types import LambdaType
 
 root=Tk()
 root.title("Simple Calculator")
-
-output=Entry(root,width=40,borderwidth=2,font=('default',12))
+root.minsize(width=400,height=400)
+root.resizable(width=False,height=False)
+canvas=Canvas(width=400,height=400,background="grey")
+canvas.pack(fill=BOTH, expand=True)
+output=Entry(canvas,width=40,font=('default',12),borderwidth=3,relief='solid')
 
 
 sign=False
@@ -83,28 +86,28 @@ def clicked(button):
     output.insert(END,button)
     
     
-one= Button(root, text="1", padx=3, pady=1, width=8, height=2, command=lambda: clicked(1))        # Lambda: is needed if arguments are passed on to a function
-two= Button(root, text="2", padx=3, pady=1, width=8, height=2, command=lambda: clicked(2))
-three= Button(root, text="3", padx=3, pady=1, width=8, height=2, command=lambda: clicked(3))
-four= Button(root, text="4", padx=3, pady=1, width=8, height=2, command=lambda: clicked(4))
-five= Button(root, text="5", padx=3, pady=1, width=8, height=2, command=lambda: clicked(5))
-six= Button(root, text="6", padx=3,  pady=1, width=8, height=2, command=lambda: clicked(6))
-seven= Button(root, text="7", padx=3, pady=1, width=8, height=2, command=lambda: clicked(7))
-eight= Button(root, text="8", padx=3, pady=1, width=8, height=2, command=lambda: clicked(8))
-nine= Button(root, text="9", padx=3, pady=1, width=8, height=2, command=lambda: clicked(9))
-zero= Button(root, text="0", padx=3, pady=1, width=8, height=2, command=lambda: clicked(0))
+one= Button(canvas, text="1", padx=3, pady=1, width=8, height=2, command=lambda: clicked(1),borderwidth=4,relief='raised')        # Lambda: is needed if arguments are passed on to a function
+two= Button(canvas, text="2", padx=3, pady=1, width=8, height=2, command=lambda: clicked(2),borderwidth=4,relief='raised')
+three= Button(canvas, text="3", padx=3, pady=1, width=8, height=2, command=lambda: clicked(3),borderwidth=4,relief='raised')
+four= Button(canvas, text="4", padx=3, pady=1, width=8, height=2, command=lambda: clicked(4),borderwidth=4,relief='raised')
+five= Button(canvas, text="5", padx=3, pady=1, width=8, height=2, command=lambda: clicked(5),borderwidth=4,relief='raised')
+six= Button(canvas, text="6", padx=3,  pady=1, width=8, height=2, command=lambda: clicked(6),borderwidth=4,relief='raised')
+seven= Button(canvas, text="7", padx=3, pady=1, width=8, height=2, command=lambda: clicked(7),borderwidth=4,relief='raised')
+eight= Button(canvas, text="8", padx=3, pady=1, width=8, height=2, command=lambda: clicked(8),borderwidth=4,relief='raised')
+nine= Button(canvas, text="9", padx=3, pady=1, width=8, height=2, command=lambda: clicked(9),borderwidth=4,relief='raised')
+zero= Button(canvas, text="0", padx=3, pady=1, width=8, height=2, command=lambda: clicked(0),borderwidth=4,relief='raised')
 
-multipy= Button(root, text="*", padx=3, pady=1, width=8, height=2, command=multiply)
-divide= Button(root, text="/",padx=3,pady=1,width=8,height=2, command=divide)
-subtract= Button(root, text="-", padx=3, pady=1, width=8, height=2, command=subtract)
-add= Button(root, text="+", padx=3, pady=1, width=8, height=2, command=add)
-equals= Button(root, text="=", padx=3, pady=1, width=8, height=2, command=equals, bg="grey")
-clear= Button(root, text="DEL", padx=3, pady=1, width=8, height=2, command=cleared, bg="red")
+multipy= Button(canvas, text="*", padx=3, pady=1, width=8, height=2, command=multiply,borderwidth=4,relief='raised')
+divide= Button(canvas, text="/",padx=3,pady=1,width=8,height=2, command=divide,borderwidth=4,relief='raised')
+subtract= Button(canvas, text="-", padx=3, pady=1, width=8, height=2, command=subtract,borderwidth=4,relief='raised')
+add= Button(canvas, text="+", padx=3, pady=1, width=8, height=2, command=add,borderwidth=4,relief='raised')
+equals= Button(canvas, text="=", padx=3, pady=1, width=8, height=2, command=equals, bg="grey",borderwidth=4,relief='raised')
+clear= Button(canvas, text="DEL", padx=3, pady=1, width=8, height=2, command=cleared, bg="red",borderwidth=4,relief='raised')
 
 
 def disp_calc(one,two,three,four,five,six,seven,eight,nine,zero,multipy,divide,subtract,add,equals,clear):
     
-    output.grid(row=0,column=0,columnspan=4,padx=3,pady=50, ipady=10)
+    output.grid(row=0,column=0,columnspan=4,padx=15,pady=50, ipady=10)
 
     add.grid(row=2,column=3)
     divide.grid(row=3,column=3)
@@ -119,6 +122,19 @@ def disp_calc(one,two,three,four,five,six,seven,eight,nine,zero,multipy,divide,s
     four.grid(row=3,column=0)
     five.grid(row=3,column=1)
     six.grid(row=3,column=2)
+
+    one.grid(row=4,column=0)
+    two.grid(row=4,column=1)
+    three.grid(row=4,column=2)
+
+    zero.grid(row=5,column=0)
+    equals.grid(row=5,column=1)
+    clear.grid(row=5,column=2)
+
+
+
+disp_calc(one,two,three,four,five,six,seven,eight,nine,zero,multipy,divide,subtract,add,equals,clear)
+root.mainloop()
 
     one.grid(row=4,column=0)
     two.grid(row=4,column=1)
